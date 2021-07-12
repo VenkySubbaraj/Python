3c066f698f26ad3257f88e4c02001608a13e27e3
venkat <venkatachalapathy@nanonino.in>
2021-07-12 08:05:10 +0530

    venkat python works

---
+++ venkat.py
@@ -0,0 +1,56 @@
+#for y in "banana":
+#    print(y);
+
+#for Z in ("apple","cherry","banana"):
+#    print(Z);
+#for x in { "name":"venkat", "age":25 }:
+#    print(name(x));
+#
+# b = """ hi venkat how are you
+# please let me the result
+# when it come then i ll bring
+# the certificates""";
+# print(b);
+#
+#
+# a = "hello! world"
+# print(a[1]);
+#
+# var1 = {"apple","banana","cherry"};
+# for x in var1:
+#     print(x);
+#     if(x == "banana"):
+#         break;
+#
+# c =  """ hi venkat how are you
+# please let me the result
+# when it come then i ll bring
+# the certificates""";
+# if "expensive" not in c:
+#     print("no expensive is not in the comment line");
+
+
+# var2 = "hello! World";
+# print(var2[2:5]);
+#
+# var3="hi venkat how are you";
+# print(var3.upper());
+#
+# var4 = "HI VENKAT HOW ARE YOU";
+# print(var4.lower());
+#
+# var5= " hi subbaraj how are you ";
+# print(var5.strip());
+
+
+var6 = " I need to replace the words as world";
+x = var6.replace("words", "world");
+print (x.strip());
+if("world" in x):
+    y = (x.replace("world","ZERO"));
+print(y.strip());
+var7 = y;
+z = (var7.replace("ZERO",","));
+print (z);
+print(z.strip());
+print(z.split(","));
