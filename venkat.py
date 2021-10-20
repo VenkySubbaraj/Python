b6a1bce97d8c09ceec04de1ea47069025df2f961
VenkatSubbaraj <venkysubbaraj@gmail.com>
2021-10-20 14:05:05 +0530

    Training python

--- venkat.py
+++ venkat.py
@@ -1,56 +1,9 @@
-#for y in "banana":
-#    print(y);
-
-#for Z in ("apple","cherry","banana"):
-#    print(Z);
-#for x in { "name":"venkat", "age":25 }:
-#    print(name(x));
-#
-# b = """ hi venkat how are you
-# please let me the result
-# when it come then i ll bring
-# the certificates""";
-# print(b);
-#
-#
-# a = "hello! world"
-# print(a[1]);
-#
-# var1 = {"apple","banana","cherry"};
-# for x in var1:
-#     print(x);
-#     if(x == "banana"):
-#         break;
-#
-# c =  """ hi venkat how are you
-# please let me the result
-# when it come then i ll bring
-# the certificates""";
-# if "expensive" not in c:
-#     print("no expensive is not in the comment line");
-
-
-# var2 = "hello! World";
-# print(var2[2:5]);
-#
-# var3="hi venkat how are you";
-# print(var3.upper());
-#
-# var4 = "HI VENKAT HOW ARE YOU";
-# print(var4.lower());
-#
-# var5= " hi subbaraj how are you ";
-# print(var5.strip());
-
-
-var6 = " I need to replace the words as world";
-x = var6.replace("words", "world");
-print (x.strip());
-if("world" in x):
-    y = (x.replace("world","ZERO"));
-print(y.strip());
-var7 = y;
-z = (var7.replace("ZERO",","));
-print (z);
-print(z.strip());
-print(z.split(","));
+import boto3
+ec2 = boto3.resource('ec2')
+instances=ec2.create_instances(
+ImageId='ami-0d382e80be7ffdae5',
+MinCount=1,
+MaxCount=1,
+InstanceType='t2.micro',
+KeyName='DockerContainer'
+)
