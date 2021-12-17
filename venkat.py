82162c97d6f64a21c1050d89dd8d731c1dd130f3
Ubuntu <azure_user@VMmachine.kgpdayncgumepi10rnlelymhwf.bx.internal.cloudapp.net>
2021-12-17 16:07:09 +0000

    new changes

--- venkat.py
+++
@@ -1,9 +0,0 @@
-import boto3
-ec2 = boto3.resource('ec2')
-instances=ec2.create_instances(
-ImageId='ami-0d382e80be7ffdae5',
-MinCount=1,
-MaxCount=1,
-InstanceType='t2.micro',
-KeyName='DockerContainer'
-)
