9734e0612898ce534cd026429722150a8b20b6ca
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-02-20 10:49:46 +0530

    Update CreatePolicy.py

--- Python_Function/CreatePolicy.py
+++ Python_Function/CreatePolicy.py
@@ -4,7 +4,7 @@ import boto3
 import json
 
 def create_role():
-    client = boto3.client('iam', aws_access_key_id = input(aws_secret_key), aws_secret_access_key = input(aws_secret_key))
+    client = boto3.client('iam', aws_access_key_id = input("aws_secret_key"), aws_secret_access_key = input("aws_secret_key"))
     try:
         print("INSIDE")
         my_managed_policy = {
