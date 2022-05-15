b722548436ae4a9a0913c6eb800e0916627de858
venkat <Venky.Subbaraj@gmail.com>
2022-05-15 11:56:06 +0530

    MD5-checksum

---
+++ MD5CheckSum/md5.py
@@ -0,0 +1,10 @@
+import hashlib
+
+with open("./test.csv", "rb") as f :
+    hash_type = hashlib.md5()
+    while temp := f.read(8192):
+        hash_type.update(temp)
+
+#print(hash_type.digest())
+print(hash_type.hexdigest())
+
