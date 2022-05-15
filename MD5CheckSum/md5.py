51b443920a9291b9647d56888503c868baaba493
venkat <Venky.Subbaraj@gmail.com>
2022-05-15 12:17:05 +0530

    checksum changes

--- MD5CheckSum/md5.py
+++ MD5CheckSum/md5.py
@@ -1,6 +1,6 @@
 import hashlib
 
-def md5():
+
 with open("./test.csv", "rb") as f :
     hash_type = hashlib.md5()
     while temp := f.read(8192):
@@ -9,5 +9,3 @@ with open("./test.csv", "rb") as f :
 #print(hash_type.digest())
 print(hash_type.hexdigest())
 
-if "__name__" = "__main__":
-    md5():
