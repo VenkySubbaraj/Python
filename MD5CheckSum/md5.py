1600d4821e2d3d69778f2b53270ef6e63dc4bc0a
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2022-05-17 17:44:43 +0530

    Update md5.py

--- MD5CheckSum/md5.py
+++ MD5CheckSum/md5.py
@@ -1,11 +1,20 @@
-import hashlib
+import hashlib 
+def algorithm():
+    with open("./test.csv", "rb") as f :
+        hash_type = hashlib.md5()
+        while temp := f.read(4082):
+            hash_type.update(temp)
+            hash_type_digest = hash_type.hexdigest()
+            #print(hash_type_digest)
+    return(hash_type_digest)
 
+def writeline():
+    file = open("./hashtag.txt", "w")
+    file.write(algorithm())
+    file.close()
+    print(file)
 
-with open("./test.csv", "rb") as f :
-    hash_type = hashlib.md5()
-    while temp := f.read(8192):
-        hash_type.update(temp)
-
-#print(hash_type.digest())
-print(hash_type.hexdigest())
-
+if __name__ == "__main__" :
+    print("INN")
+    print(algorithm())
+    writeline()
