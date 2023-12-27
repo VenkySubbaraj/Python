c9569862f8fdf6de068e554e862e0213c9b992ab
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2023-12-27 09:47:55 +0530

    Update basic_glue_job.tf

--- rifaz_work/assignment3/basic_glue_job.tf
+++ rifaz_work/assignment3/basic_glue_job.tf
@@ -1,7 +1,10 @@
 resource "aws_glue_job" "sample_glue_job" {
   name          = "sample_glue_job"
-  role_arn      = aws_iam_role.glue_role.arn
-  command       = "glueetl"
-  script        = file("path/to/your/glue_script.py")  # Path to your PySpark script
-  python_version = "3"
+  role_arn      = "arn:aws:iam::667289912626:role/testing1_role"
+  glue_version  = "4.0"
+command {
+
+    script_location = "s3://rifazdec24/pyspark.py"
+    python_version = "3"
+  }
 }
