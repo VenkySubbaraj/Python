4c31c898ae95bf92774b82fb3e2771ac2e46bbce
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2023-12-27 09:46:15 +0530

    Update Schedule ETL_Job.py

--- rifaz_work/assignemnt4/Schedule ETL_Job.py
+++ rifaz_work/assignemnt4/Schedule ETL_Job.py
@@ -4,21 +4,15 @@ from pyspark.sql.functions import lit
 # Initialize Spark session
 spark = SparkSession.builder.appName("S3ReadWriteExample").getOrCreate()
 
-aws_access_key_id = input("access_key")
-aws_secret_access_key = input("secret_key")
-
-spark.conf.set("spark.hadoop.fs.s3a.access.key", aws_access_key_id)
-spark.conf.set("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key)
-
-s3_input_path = "s3a://your_bucket/your_input_path/input.csv"
-s3_output_path = "s3a://your_bucket/your_output_path/output.csv"
+s3_input_path = "s3://rifazdec24/sample_date.csv"
+s3_output_path = "s3://rifazdec24/output/sample_date.csv"
 
 df = spark.read.csv(s3_input_path, header=True, inferSchema=True)
 
 
-df = df.withColumn("column_name here", lit(1))
+df = df.withColumn("rifaz", lit(1))
 df.show()
 
-df.coalesce(1).write.mode("overwrite").csv(s3_output_path)
+df.coalesce(1).write.mode("overwrite").csv(s3_output_path).option("header","true")
 
 spark.stop()
