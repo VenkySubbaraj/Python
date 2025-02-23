5024c51ad9630b9eb60eaed32730765bd9399334
VenkatachalapathySubbaraj <87265462+VenkySubbaraj@users.noreply.github.com>
2025-02-23 15:37:49 +0530

    Delete Windows directory

--- Windows/aws-cli.sh
+++
@@ -1,5 +0,0 @@
-########################################################
-####  To generate the access token for 24 hours  #######
-########################################################
-
-aws sts get-session-token --duration-seconds 86400
